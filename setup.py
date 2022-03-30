63f72f6c0eaa986321387a7999f7d0ca451d1920
Mufeed VH <mufeedvh@gmail.com>
2022-03-31 00:30:19 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     # package
     name = 'okjson',
@@ -12,10 +15,17 @@ setup(
     # metadata
     version = '0.1.0',
     description = 'A fast, simple, and pythonic JSON Schema Validator.',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
     author = 'Mufeed VH',
     author_email = 'contact@mufeedvh.com',
     url = 'https://github.com/mufeedvh/okjson',
     download_url = 'https://github.com/mufeedvh/okjson/archive/refs/tags/v0.1.0.tar.gz',
     keywords = ['json', 'schema', 'jsonschema', 'validation', 'validator'],
-    license = 'MIT'
+    license = 'MIT',
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ], 
 )
